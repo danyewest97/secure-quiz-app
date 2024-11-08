fe2f84eea5c7c36dd6084048c1b50993e89d6f79
danyewest97 <dane20052008@gmail.com>
2024-11-08 13:53:22 -0800

    quick commit

--- app.py
+++ app.py
@@ -4,16 +4,28 @@ import random, string, json, os
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
 
+app.secret_key=os.environ["SECRET_KEY"]; #secret key, very secret, make a new one using python -c "import secrets; print(secrets.token_hex())" in cmd prompt
+#stored in key.txt
 
 @app.route("/")
 def render_start():
-    print(getQuestion(0))
     return render_template('start.html')
 
-# @app.route("/nextquestion")
-# def render_start():
-    # return render_template('question.html')
+@app.route("/next")
+def render_start():
+    return render_template('question.html')
 
+
+
+
+
+
+
+def checkAnswer(questionIndex, givenAnswer):
+    question = getQuestion(questionIndex)
+    rightAnswer = question["correct"]
+    
+    return givenAnswer == rightAnswer
 
 
 def getQuestion(index):
@@ -21,6 +33,9 @@ def getQuestion(index):
         questions = json.load(data)
     
     return questions[index]
+
+
+
 
 
 if __name__=="__main__":
