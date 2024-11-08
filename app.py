bb8f3b06b76aa288e139048ee107f891e4292c8a
danyewest97 <dane20052008@gmail.com>
2024-11-08 13:47:54 -0800

    quick commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, url_for, render_template, request
-import random, string
+import random, string, json, os
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
@@ -7,9 +7,20 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 
 @app.route("/")
 def render_start():
+    print(getQuestion(0))
     return render_template('start.html')
 
+# @app.route("/nextquestion")
+# def render_start():
+    # return render_template('question.html')
 
+
+
+def getQuestion(index):
+    with open("static/questions.json") as data:
+        questions = json.load(data)
+    
+    return questions[index]
 
 
 if __name__=="__main__":
